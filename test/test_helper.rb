b20e6162229fc72820eca0dc99d89b969e2ab860
Simon Rabuogi <symokevo@gmail.com>
2024-04-16 19:07:53 +0300

    Complete advanced testing setup

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,18 @@
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
+require "minitest/reporters"
+Minitest::Reporters.use!
+
+class ActiveSupport::TestCase
+  # Run tests in parallel with specified workers
+  parallelize(workers: :number_of_processors)
+
+  # Set up all fixtures in test/fixtures/*.yml.
+  fixtures :all
+
+  # More helper methods to be used here...
+end
 
 module ActiveSupport
   class TestCase
