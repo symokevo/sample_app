d19880c4a377df68e510884f3ade338d9abc856c
Simon Rabuogi <symokevo@gmail.com>
2024-04-16 16:59:42 +0300

    Finish Static pages
    * Add more About and Contact static_pages
    * Write test for these pages
    * Fail the test for the pages
    * Implement the code to make the tests pass
    * Refactor the code

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
+  get 'static_pages/contact'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -11,5 +13,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
-  root "application#index"
+  root "static_pages#home"
 end
