cda82d4902de5fc04b48d678a06798f345a3d1d6
Simon Rabuogi <symokevo@gmail.com>
2024-04-23 18:35:39 +0300

    Add ssl and prepare puma server for production

--- config/puma.rb
+++ config/puma.rb
@@ -26,10 +26,14 @@ worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 port ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
-environment ENV.fetch("RAILS_ENV") { "development" }
+environment ENV.fetch("RAILS_ENV") { ENV['RACK_ENV'] || "development" }
 
 # Specifies the `pidfile` that Puma will use.
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
+
+workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+
+preload_app!
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
