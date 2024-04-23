91ada5b4efa52d43a6de27cb052cf8c349387169
Simon Rabuogi <symokevo@gmail.com>
2024-04-23 06:44:43 +0300

    Handle unsuccessful Signups
    - create an error handling partial
    - Render the partial in the new user form
    - Format the error messages with extended css in the custom sass file

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,4 @@
 class UsersController < ApplicationController
-  def new
-  end
-
   def show
     @user = User.find(params[:id])
     # debugger
@@ -9,5 +6,20 @@ class UsersController < ApplicationController
 
   def new
     @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # Handle a successful save
+    else
+      # return status code 422
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
