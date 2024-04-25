8ebd39c8141a4e0b7c8affa0257a604611c820eb
Simon Rabuogi <symokevo@gmail.com>
2024-04-25 07:53:30 +0300

    Write Integration Test for users_login_test
    - rails g integration_test users_login
    - Write failing tests
    - Write code to make them pass

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page.
     else
+      flash.now[:danger] = 'Invalid Email/Password Combination'
       # Creat an error message.
       render 'new', status: :unprocessable_entity
     end
