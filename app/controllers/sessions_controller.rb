e81d176ca8e6f1cf3b69cf91e598727d892cd054
Simon Rabuogi <symokevo@gmail.com>
2024-04-26 17:18:27 +0300

    Test Remember me checkbox

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
     if user&.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page.
       reset_session # Helps defend the system against SESSION FIXATION ATTACKS
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user) # checks whether the user opts to be remembered at login.
       remember user
       log_in user
       redirect_to user
@@ -16,6 +17,19 @@ class SessionsController < ApplicationController
       render 'new', status: :unprocessable_entity
     end
   end
+
+  # def create
+  #   @user = User.find_by(email: params[:session][:email].downcase)
+  #   if @user && @user.authenticate(params[:session][:password])
+  #     reset_session # Helps defend the system against SESSION FIXATION ATTACKS
+  #     params[:session][:remember_me] = '1' ? remember(@user) : forget(@user)
+  #     log_in @user
+  #     redirect_to @user
+  #   else
+  #     flash.now[:danger] = 'Invalid email/password combination'
+  #     render 'new', status: :unprocessable_entity
+  #   end
+  # end
 
   def destroy
     log_out if logged_in?
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -8,6 +8,20 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   def setup
     @user = user(:stc) # fixture from test/fixtures/users.rb
   end
+
+  test "login with remembering" do
+    log_in_as(@user, remember_me: '1')
+    assert_not cookies[:remember_token].blank?
+    # asser_equal FILL_IN, assigns(:user).FILL_IN
+  end
+
+  test "login without remembering" do
+    # Log in to set the cookie.
+    log_in_as(@user, remember_me: '1')
+    # Log in again and verify that the cookie is deleted.
+    log_in_as(@user, remember_me: '0')
+    assert cookies[:remember_token].blank?
+  end
 end
 
 class InvalidPasswordTest < UsersLoginTest
