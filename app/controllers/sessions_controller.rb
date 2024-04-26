b2a762fd3dc008b35aa11f8579e010acabe10cfe
Simon Rabuogi <symokevo@gmail.com>
2024-04-26 13:24:43 +0300

    Engineer Login with Remembering
    - Add an authenticated? method to the User model
    - Enable logging in and remembering in the sessions_controller.rb
    - Enable remembering the user in session_helper.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
     if user&.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page.
       reset_session # Helps defend the system against SESSION FIXATION ATTACKS
+      remember user
       log_in user
       redirect_to user
     else
