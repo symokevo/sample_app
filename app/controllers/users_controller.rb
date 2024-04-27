5a801a9b1106a0f8f9c4ed4df67ee03611b04ec6
Simon Rabuogi <symokevo@gmail.com>
2024-04-27 22:43:45 +0300

    Create the Destroy action in the Users controller
    - Add the destroy action to the before_action filter in the Users controller
    - Restrict destroy action to admins

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
 
   def index
     # implement pagination
@@ -46,6 +47,13 @@ class UsersController < ApplicationController
     end
   end
 
+  # Deleting users
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url, status: :see_other
+  end
+
   private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
@@ -65,5 +73,10 @@ class UsersController < ApplicationController
   def correct_user
     @user = User.find(params[:id])
     redirect_to(root_url, status: :see_other) unless current_user?(@user)
+  end
+
+  # Confirms an admin user.
+  def admin_user
+    redirect_to(root_url, status: :see_other) unless current_user.admin?
   end
 end
