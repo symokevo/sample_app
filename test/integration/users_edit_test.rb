56ecdb3faebdc314b8e8f37e6d429a0947ed1b25
Simon Rabuogi <symokevo@gmail.com>
2024-04-27 09:25:15 +0300

    Implement a mechanism restriction users to beable to update and edit profile only upon loggin in

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -5,6 +5,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     @user = users(:stc)
   end
   test "unsuccessful edit" do
+    log_in_as(@user)
     get edit_user_path(@user)
     assert_template 'users/edit'
     patch user_path(@user), params: { user: { name: "",
@@ -15,7 +16,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end
 
   test "successful edit" do
-
+    log_in_as(@user)
     get edit_user_path(@user)
     assert_template 'users/edit'
     name = "Foo Bar"
