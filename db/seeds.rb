8ea3f1c4af474eeec703f73b04aab841a7abf006
Simon Rabuogi <symokevo@gmail.com>
2024-04-28 10:13:28 +0300

    Start Implementing Account
    - Create account activation resource
    - Create account activation migration with activation_digest and activated columns
    - Add activation_digest to user model
    - Add account activation to user fixtures
    - Add account activation to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,21 @@
 # Create a main sample user.
-User.create!( name: "Example User", email: "simon@suuef.com", password: "foobar", password_confirmation: "foobar", admin: true )
+User.create!( name: "Example User",
+              email: "simon@suuef.com",
+              password: "foobar",
+              password_confirmation: "foobar",
+              admin: true,
+              activated: true,
+              activated_at: Time.zone.now )
 
 # Generate a bunch of additional users.
 99.times do |n|
   name = Faker::Name.name
   email = "example-#{n+1}@suuef.com"
   password = "password"
-  User.create!( name: name, email: email, password: password, password_confirmation: password )
+  User.create!( name: name,
+                email: email,
+                password: password,
+                password_confirmation: password,
+                activated: true,
+                activated_at: Time.zone.now )
 end
