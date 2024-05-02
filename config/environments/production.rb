cbf2ab74540541a387c0637bdf371016d8831ff8
Simon Rabuogi <symokevo@gmail.com>
2024-05-02 17:17:17 +0300

    Reconfigure sendgrid api

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgird.net',
-    :port           => '587',
+    :port           => '465',
     :authentication => :plain,
     :user_name      => 'apikey',
     :password       => ENV['SENDGRID_API_KEY'],
