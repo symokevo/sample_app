47fcdee4128c3da8e74570fce5f857dda5b82966
Simon Rabuogi <symokevo@gmail.com>
2024-05-02 12:42:06 +0300

    sendgrid configuration

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgird.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => 'apikey',
+  :password       => ENV['SENDGRID_API_KEY'],
+  :domain         => '127.0.0.1',
+  :enable_starttls_auto => true
+}
