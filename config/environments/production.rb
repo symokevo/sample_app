aaadfae436579d68c58012fbb573a0219eb0b394
Simon Rabuogi <symokevo@gmail.com>
2024-05-03 16:15:26 +0300

    Update production configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,15 +69,6 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   host = 'https://web-production-2368c.up.railway.app'
   config.action_mailer.default_url_options = { host: host }
-  # ActionMailer::Base.smtp_settings = {
-  #   :address        => 'smtp.gmail.com',
-  #   :port           => 587,
-  #   :authentication => :plain,
-  #   :user_name      => Rails.application.credentials.database[:username],
-  #   :password       => Rails.application.credentials.database[:GMAIL_PASSWORD],
-  #   :domain         => 'web-production-2368c.up.railway.app',
-  #   :enable_starttls_auto => true
-  # }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
