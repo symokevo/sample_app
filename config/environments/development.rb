7557e81445a2190ec9d26062cb7e3400486e6753
Simon Rabuogi <symokevo@gmail.com>
2024-05-05 22:25:36 +0300

    Refactor config/environments/production.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,9 +36,6 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = true
-
   host = '127.0.0.1'
   # Use this if developing on localhost.
   config.action_mailer.default_url_options = { host: host, port: 3000 }
