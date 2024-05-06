58205969f3f4e76d41164e48cf3d673fb0ecc950
Simon Rabuogi <symokevo@gmail.com>
2024-05-06 08:01:04 +0300

    Update smtp config settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_caching = false
-  config.action_mailer.smtp_settings = {
+  ActionMailer::Base.smtp_settings = {
     address:         'smtp.gmail.com',
     port:            587,
     user_name:       ENV['GOOGLE_SMTP_GMAIL'],
