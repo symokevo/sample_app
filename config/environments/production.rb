a66d7d96b87d1a24306106769c93926cca066f45
Simon Rabuogi <symokevo@gmail.com>
2024-05-06 06:07:25 +0300

    Update production env configuration for railway.app

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,9 +64,9 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  host = '127.0.0.1'
+  host = 'web-production-2368c.up.railway.app'
   # Use this if developing on localhost.
-  config.action_mailer.default_url_options = { host: host, port: 3000 }
+  config.action_mailer.default_url_options = { host: host }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
