d1f977dbe601a1baee9debd40c01399a2533eee2
Simon Rabuogi <symokevo@gmail.com>
2024-05-06 06:17:44 +0300

    update production environment host variable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,9 +62,8 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "sample_app_production"
 
-  # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  config.action_mailer.default_url_options = { host: 'web-production-2368c.up.railway.app' }
+  host = 'web-production-2368c.up.railway.app'
+  config.action_mailer.default_url_options = { host: host }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
