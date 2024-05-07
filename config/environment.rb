bf295e5e43551c7a109c551f8f9ff68f6389d96b
Simon Rabuogi <symokevo@gmail.com>
2024-05-07 08:13:06 +0300

    Edit production settings to try smtp functionality

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,12 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+    address:         'smtp.gmail.com',
+    port:            587,
+    user_name:       ENV['GOOGLE_SMTP_GMAIL'],
+    password:        ENV['GOOGLE_SMTP_PASSWORD'],
+    authentication: "plain",
+    enable_starttls_auto: true
+  }
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,14 +68,14 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_caching = false
-  ActionMailer::Base.smtp_settings = {
-    address:         'smtp.gmail.com',
-    port:            587,
-    user_name:       ENV['GOOGLE_SMTP_GMAIL'],
-    password:        ENV['GOOGLE_SMTP_PASSWORD'],
-    authentication: "plain",
-    enable_starttls_auto: true
-  }
+  # ActionMailer::Base.smtp_settings = {
+  #   address:         'smtp.gmail.com',
+  #   port:            587,
+  #   user_name:       ENV['GOOGLE_SMTP_GMAIL'],
+  #   password:        ENV['GOOGLE_SMTP_PASSWORD'],
+  #   authentication: "plain",
+  #   enable_starttls_auto: true
+  # }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
